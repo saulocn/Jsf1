8125e60e28b36d49405e7ec3b7b740cd830ad161
saulo <saulocn@gmail.com>
2016-06-28 09:39:26 -0300

    - Temas dinâmicos;

--- src/br/com/caelum/livraria/bean/TemaBean.java
+++ src/br/com/caelum/livraria/bean/TemaBean.java
@@ -6,7 +6,7 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class TemaBean {
-	
+
 	private String tema = "vader";
 
 	public String getTema() {
@@ -15,6 +15,14 @@ public class TemaBean {
 
 	public void setTema(String tema) {
 		this.tema = tema;
+	}
+
+	public String[] getTemas() {
+		return new String[] { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer", "bluesky",
+				"bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv", "eggplant",
+				"excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+				"overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness", "south-street", "start",
+				"sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };	
 	}
 
 }
